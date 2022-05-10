0c2ffe047264905b1e0c056fff6beb06686df0ec
Jacob Ley <jacob@getshowcase.io>
2022-05-10 09:53:38 -0400

    Migrate enum to array

---
+++ packages/enum-to-array/README.md
@@ -0,0 +1,105 @@
+<div style="text-align:center">
+
+<h1>enum-to-array</h1>
+<p>Convert Typescript Enums to a strongly typed array.</p>
+
+[![npm package](https://badge.fury.io/js/enum-to-array.svg)](https://www.npmjs.com/package/enum-to-array)
+[![License](https://img.shields.io/github/license/JacobLey/jacobley.svg)](https://github.com/JacobLey/jacobley/blob/main/LICENSE)
+[![Quality](https://img.shields.io/npms-io/quality-score/enum-to-array)](https://www.npmjs.com/package/enum-to-array)
+
+</div>
+
+## Contents
+- [Introduction](#introduction)
+- [Install](#install)
+- [Example](#example)
+- [Usage](#usage)
+- [API](#api)
+
+<a name="Introduction"></a>
+## Introduction
+
+Enum to array is a light module for converting a typescript enum to an array of keys or values.
+
+Thanks to [reverse mappings](https://www.typescriptlang.org/docs/handbook/enums.html#reverse-mappings) this is a little bit trickier than `Object.keys` or `Object.values`
+
+<a name="Install"></a>
+## Install
+
+```sh
+npm i enum-to-array
+```
+
+<a name="Example"></a>
+## Example
+
+```ts
+import { enumToValues } from 'enum-to-values';
+
+enum MyEnum {
+    FOO = 'BAR',
+    ABC = 123,
+}
+console.log(enumToArray(MyEnum));
+// [{ key: 'FOO', value: 'BAR' }, { key: 'ABC', value: 123 }]
+```
+
+<a name="usage"></a>
+## Usage
+
+enum-to-array is an ESM module. That means it _must_ be `import`ed. To load from a CJS module, use dynamic import `const { enumToArray } = await import('enum-to-array');`.
+
+Due to the nature of `const enum`s, those are not usable with this library as there is nothing to pass at runtime.
+
+This can be partially resolved by using [preserveConstEnums](https://www.typescriptlang.org/tsconfig#preserveConstEnums).
+
+<a name="api"></a>
+## API
+
+enum-to-array exports three functions:
+
+### enumToArray
+Lists key-value pairs of enum, in order that they occur.
+
+```ts
+import { enumToArray } from 'enum-to-array';
+
+enum MyEnum {
+    FOO = 'BAR',
+    ABC = 123,
+    DUP = FOO,
+}
+console.log(enumToArray(MyEnum));
+// [
+//    { key: 'FOO', value: 'BAR' },
+//    { key: 'ABC', value: 123 },
+//    { key: 'DUP', value: 'BAR' },
+// ]
+```
+
+### enumToValues
+Lists values of enum, in order that they occur.
+
+```ts
+import { enumToValues } from 'enum-to-array';
+
+console.log(enumToValues(MyEnum));
+// ['BAR', 123, 'BAR']
+```
+
+Optionally de-dupe values with `{ unique: true }` provided as a second parameter.
+
+```ts
+console.log(enumToValues(MyEnum, { unique: true }));
+// ['BAR', 123]
+```
+
+### enumToKeys
+Lists keys of enum, in order that they occur.
+
+```ts
+import { enumToKeys } from 'enum-to-array';
+
+console.log(enumToKeys(MyEnum));
+// ['FOO', 'ABC', 'DUP']
+```
---
+++ packages/enum-to-array/src/tests/unit/enum-to-array.spec.ts
@@ -0,0 +1,350 @@
+import { expect } from 'chai';
+import { expectTypeOf } from 'expect-type';
+import * as EnumToArray from '../../enum-to-array.js';
+
+/* eslint-disable no-restricted-syntax */
+// This test suite implements multiple enum anti-patterns
+// to ensure it works as intended, hence eslint-disable abuse.
+
+export const EnumToArraySpec = {
+    enumToArray: {
+        'Default values'() {
+            enum Default {
+                /* eslint-disable @typescript-eslint/prefer-enum-initializers */
+                A,
+                B,
+                C,
+                D,
+                /* eslint-enable @typescript-eslint/prefer-enum-initializers */
+            }
+
+            expectTypeOf(EnumToArray.enumToArray(Default)).toEqualTypeOf<
+                ({
+                    key: 'A';
+                    value: Default.A;
+                } | {
+                    key: 'B';
+                    value: Default.B;
+                } | {
+                    key: 'C';
+                    value: Default.C;
+                } | {
+                    key: 'D';
+                    value: Default.D;
+                })[]
+            >();
+            expect(EnumToArray.enumToArray(Default)).to.deep.equal([
+                { key: 'A', value: Default.A },
+                { key: 'B', value: Default.B },
+                { key: 'C', value: Default.C },
+                { key: 'D', value: Default.D },
+            ]);
+
+            expectTypeOf(EnumToArray.enumToValues(Default)).toEqualTypeOf<Default[]>();
+            expectTypeOf(EnumToArray.enumToValues(Default)).toEqualTypeOf<number[]>();
+            expect(EnumToArray.enumToValues(Default)).to.deep.equal([
+                Default.A,
+                Default.B,
+                Default.C,
+                Default.D,
+            ]);
+
+            expectTypeOf(EnumToArray.enumToKeys(Default)).toEqualTypeOf<(keyof typeof Default)[]>();
+            expectTypeOf(EnumToArray.enumToKeys(Default)).toEqualTypeOf<
+                ('A' | 'B' | 'C' | 'D')[]
+            >();
+            expect(EnumToArray.enumToKeys(Default)).to.deep.equal(['A', 'B', 'C', 'D']);
+        },
+
+        'String literals'() {
+            enum Opposite {
+                FOO = 'BAR',
+                ABC = 'XYZ',
+                YES = 'NO',
+                ONE = 'ZERO',
+            }
+
+            expectTypeOf(EnumToArray.enumToArray(Opposite)).toEqualTypeOf<
+                ({
+                    key: 'ABC';
+                    value: Opposite.ABC;
+                } | {
+                    key: 'FOO';
+                    value: Opposite.FOO;
+                } | {
+                    key: 'ONE';
+                    value: Opposite.ONE;
+                } | {
+                    key: 'YES';
+                    value: Opposite.YES;
+                })[]
+            >();
+            expect(EnumToArray.enumToArray(Opposite)).to.deep.equal([
+                { key: 'FOO', value: Opposite.FOO },
+                { key: 'ABC', value: Opposite.ABC },
+                { key: 'YES', value: Opposite.YES },
+                { key: 'ONE', value: Opposite.ONE },
+            ]);
+
+            expectTypeOf(EnumToArray.enumToValues(Opposite)).toEqualTypeOf<Opposite[]>();
+            expect(EnumToArray.enumToValues(Opposite)).to.deep.equal([
+                Opposite.FOO,
+                Opposite.ABC,
+                Opposite.YES,
+                Opposite.ONE,
+            ]);
+
+            expectTypeOf(EnumToArray.enumToKeys(Opposite)).toEqualTypeOf<
+                (keyof typeof Opposite)[]
+            >();
+            expectTypeOf(EnumToArray.enumToKeys(Opposite)).toEqualTypeOf<
+                ('ABC' | 'FOO' | 'ONE' | 'YES')[]
+            >();
+            expect(EnumToArray.enumToKeys(Opposite)).to.deep.equal(['FOO', 'ABC', 'YES', 'ONE']);
+        },
+
+        'keys are values'() {
+            enum Colors {
+                WHITE = 'WHITE',
+                BLACK = 'BLACK',
+                BLUE = 'BLUE',
+            }
+
+            expectTypeOf(EnumToArray.enumToArray(Colors)).toEqualTypeOf<
+                ({
+                    key: 'BLACK';
+                    value: Colors.BLACK;
+                } | {
+                    key: 'BLUE';
+                    value: Colors.BLUE;
+                } | {
+                    key: 'WHITE';
+                    value: Colors.WHITE;
+                })[]
+            >();
+            expect(EnumToArray.enumToArray(Colors)).to.deep.equal([
+                { key: 'WHITE', value: Colors.WHITE },
+                { key: 'BLACK', value: Colors.BLACK },
+                { key: 'BLUE', value: Colors.BLUE },
+            ]);
+
+            expectTypeOf(EnumToArray.enumToValues(Colors)).toEqualTypeOf<Colors[]>();
+            expect(EnumToArray.enumToValues(Colors)).to.deep.equal([
+                Colors.WHITE,
+                Colors.BLACK,
+                Colors.BLUE,
+            ]);
+
+            expectTypeOf(EnumToArray.enumToKeys(Colors)).toEqualTypeOf<(keyof typeof Colors)[]>();
+            expect(EnumToArray.enumToKeys(Colors)).to.deep.equal(['WHITE', 'BLACK', 'BLUE']);
+        },
+
+        'Keys and values do not match'() {
+            enum Colors {
+                WHITE = 'BLUE',
+                BLACK = 'WHITE',
+                BLUE = 'BLACK',
+            }
+
+            expectTypeOf(EnumToArray.enumToArray(Colors)).toEqualTypeOf<
+                ({
+                    key: 'BLACK';
+                    value: Colors.BLACK;
+                } | {
+                    key: 'BLUE';
+                    value: Colors.BLUE;
+                } | {
+                    key: 'WHITE';
+                    value: Colors.WHITE;
+                })[]
+            >();
+            expect(EnumToArray.enumToArray(Colors)).to.deep.equal([
+                { key: 'WHITE', value: Colors.WHITE },
+                { key: 'BLACK', value: Colors.BLACK },
+                { key: 'BLUE', value: Colors.BLUE },
+            ]);
+
+            expectTypeOf(EnumToArray.enumToValues(Colors)).toEqualTypeOf<Colors[]>();
+
+            expectTypeOf(EnumToArray.enumToKeys(Colors)).toEqualTypeOf<(keyof typeof Colors)[]>();
+        },
+
+        'Duplicate values'() {
+            enum Duplicates {
+                /* eslint-disable @typescript-eslint/no-duplicate-enum-values */
+                ONE = 1,
+                WON = 1,
+                TOO = 'TOO',
+                TWO = 'TOO',
+                FOUR = 'FOR',
+                FOR = 'FORD',
+                // eslint-disable-next-line @typescript-eslint/prefer-literal-enum-member
+                FORE = FOUR,
+                /* eslint-enable @typescript-eslint/no-duplicate-enum-values */
+            }
+
+            expectTypeOf(EnumToArray.enumToArray(Duplicates)).toEqualTypeOf<
+                ({
+                    key: 'FOR';
+                    value: Duplicates.FOR;
+                } | {
+                    key: 'FORE';
+                    value: Duplicates.FORE;
+                } | {
+                    key: 'FOUR';
+                    value: Duplicates.FOUR;
+                } | {
+                    key: 'ONE';
+                    value: Duplicates.ONE;
+                } | {
+                    key: 'TOO';
+                    value: Duplicates.TOO;
+                } | {
+                    key: 'TWO';
+                    value: Duplicates.TWO;
+                } | {
+                    key: 'WON';
+                    value: Duplicates.WON;
+                })[]
+            >();
+            expect(EnumToArray.enumToArray(Duplicates)).to.deep.equal([
+                { key: 'ONE', value: Duplicates.ONE },
+                { key: 'WON', value: Duplicates.WON },
+                { key: 'TOO', value: Duplicates.TOO },
+                { key: 'TWO', value: Duplicates.TWO },
+                { key: 'FOUR', value: Duplicates.FOUR },
+                { key: 'FOR', value: Duplicates.FOR },
+                { key: 'FORE', value: Duplicates.FORE },
+            ]);
+
+            expectTypeOf(EnumToArray.enumToValues(Duplicates)).toEqualTypeOf<Duplicates[]>();
+            expect(EnumToArray.enumToValues(Duplicates, { unique: true })).to.deep.equal([
+                Duplicates.ONE,
+                Duplicates.TOO,
+                Duplicates.FOUR,
+                Duplicates.FOR,
+            ]);
+
+            expectTypeOf(EnumToArray.enumToKeys(Duplicates)).toEqualTypeOf<
+                (keyof typeof Duplicates)[]
+            >();
+            expectTypeOf(EnumToArray.enumToKeys(Duplicates)).toEqualTypeOf<
+                ('FOR' | 'FORE' | 'FOUR' | 'ONE' | 'TOO' | 'TWO' | 'WON')[]
+            >();
+        },
+
+        'Computed values'() {
+            enum Computed {
+                /* eslint-disable @typescript-eslint/prefer-literal-enum-member */
+                FIVE = 5,
+                DOUBLE = FIVE * 2,
+                HALF = DOUBLE / 2,
+                NAN = Number.NaN,
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                Infinity = Number.POSITIVE_INFINITY,
+                /* eslint-enable @typescript-eslint/prefer-literal-enum-member */
+            }
+
+            expectTypeOf(EnumToArray.enumToArray(Computed)).toEqualTypeOf<
+                ({
+                    key: 'DOUBLE';
+                    value: typeof Computed.DOUBLE;
+                } | {
+                    key: 'FIVE';
+                    value: typeof Computed.FIVE;
+                } | {
+                    key: 'HALF';
+                    value: typeof Computed.HALF;
+                } | {
+                    key: 'Infinity';
+                    // eslint-disable-next-line unicorn/prefer-number-properties
+                    value: typeof Computed.Infinity;
+                } | {
+                    key: 'NAN';
+                    value: typeof Computed.NAN;
+                })[]
+            >();
+            expect(EnumToArray.enumToArray(Computed)).to.deep.equal([
+                { key: 'FIVE', value: Computed.FIVE },
+                { key: 'DOUBLE', value: Computed.DOUBLE },
+                { key: 'HALF', value: Computed.HALF },
+                { key: 'NAN', value: Computed.NAN },
+                { key: 'Infinity', value: Computed.Infinity },
+            ]);
+
+            expectTypeOf(EnumToArray.enumToValues(Computed)).toEqualTypeOf<Computed[]>();
+            expect(EnumToArray.enumToValues(Computed, { unique: true })).to.deep.equal([
+                Computed.FIVE,
+                Computed.DOUBLE,
+                Computed.NAN,
+                Computed.Infinity,
+            ]);
+
+            expectTypeOf(EnumToArray.enumToKeys(Computed)).toEqualTypeOf<
+                (keyof typeof Computed)[]
+            >();
+        },
+
+        'Values are computed literals'() {
+            const set = new Set([1, 2, 3]);
+            const reg = /abc/u;
+            enum Literals {
+                /* eslint-disable @typescript-eslint/prefer-literal-enum-member */
+                // @ts-expect-error
+                SET = set,
+                // @ts-expect-error
+                OBJ = {},
+                // @ts-expect-error
+                ARR = [],
+                // @ts-expect-error
+                NIL = null,
+                // @ts-expect-error
+                REG = reg,
+                /* eslint-enable @typescript-eslint/prefer-literal-enum-member */
+            }
+
+            expectTypeOf(EnumToArray.enumToArray(Literals)).toEqualTypeOf<
+                ({
+                    key: 'ARR';
+                    value: typeof Literals.ARR;
+                } | {
+                    key: 'NIL';
+                    value: typeof Literals.NIL;
+                } | {
+                    key: 'OBJ';
+                    value: typeof Literals.OBJ;
+                } | {
+                    key: 'REG';
+                    value: typeof Literals.REG;
+                } | {
+                    key: 'SET';
+                    value: typeof Literals.SET;
+                })[]
+            >();
+            expect(EnumToArray.enumToArray(Literals)).to.deep.equal([
+                { key: 'SET', value: Literals.SET },
+                { key: 'OBJ', value: Literals.OBJ },
+                { key: 'ARR', value: Literals.ARR },
+                { key: 'NIL', value: Literals.NIL },
+                { key: 'REG', value: Literals.REG },
+            ]);
+
+            expectTypeOf(EnumToArray.enumToValues(Literals)).toEqualTypeOf<Literals[]>();
+
+            expectTypeOf(EnumToArray.enumToKeys(Literals)).toEqualTypeOf<
+                (keyof typeof Literals)[]
+            >();
+        },
+
+        'Empty enum'() {
+            enum Empty {}
+
+            expectTypeOf(EnumToArray.enumToArray(Empty)).toEqualTypeOf<[]>();
+            expect(EnumToArray.enumToArray(Empty)).to.deep.equal([]);
+
+            expectTypeOf(EnumToArray.enumToValues(Empty)).toEqualTypeOf<[]>();
+
+            expectTypeOf(EnumToArray.enumToKeys(Empty)).toEqualTypeOf<[]>();
+        },
+    },
+};
