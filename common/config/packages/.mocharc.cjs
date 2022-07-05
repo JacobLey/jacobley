1a8db14c91086ad7aeb0fa55be8f9ca4c3724236
Jacob Ley <jacob@getshowcase.io>
2022-07-05 14:00:58 -0400

    Allow custom `nyc` imports

--- common/config/packages/.mocharc.cjs
+++ common/config/packages/.mocharc.cjs
@@ -1,7 +1,17 @@
 'use strict';
 
+const nodeOptions = ['conditions=patchable'];
+
+// eslint-disable-next-line node/no-process-env
+const nycCwd = process.env.NYC_CWD;
+
+if (nycCwd) {
+    const packageName = nycCwd.split('/').pop();
+    nodeOptions.push(`conditions=nyc-${packageName}`);
+}
+
 module.exports = {
-    'node-option': ['conditions=patchable'],
+    'node-option': nodeOptions,
     'recursive': true,
     'ui': 'exports',
 };
